d4084dfd1aae41231cee5bfeb171392a6e60430c
Miles J. Litteral <Mandaloe2@gmail.com>
2023-01-22 11:55:47 -0800

    Update Main.hs

--- app/Main.hs
+++ app/Main.hs
@@ -21,19 +21,11 @@ main = do
 
     let united  = KeyStore [("first", BS.empty), ("second", BS.empty), ("third", BS.empty)]
         testSet = KeyStore [("s1", a1), ("s2", a2), ("s3", a3), ("s4", a4), ("s5", a5), ("s6", a6), ("s7", a7), ("s8", a8), ("s9", a9), ("s10", a10)]
-    --eventMessageS LOG_HEAD "Welcome"
-    eventMessageS "WELCOME"
-    debugMessageS LOG_HEAD "Start KeyStore I/O"
-    reportMessageS LOG_BODY "Loading Images"
-    orderedMessageS LOG_BODY "Saving KeyStore: United as testA (empty store)"
+    print "Saving KeyStore: United as testA (empty store)"
     saveStore "./testGround/testA"   united  -- empty keystore
-    orderedMessageS LOG_BODY   "Saving KeyStore: testSet as testSet (real store)"
     saveStore "./testGround/testSet" testSet -- keystore of images
-    orderedMessageS LOG_BODY "Loading KeyStore testSet"
+    print "Loading KeyStore testSet"
     loadedContents <- loadStore "./testGround/testSet.keystore"
-    infoMessageS LOG_BODY "autoUnpack into ./testGround"
+    print "autoUnpack into ./testGround"
     autoUnpack "./testGround" loadedContents
-    debugMessageS LOG_BODY "done"
-    infoMessageS LOG_TAIL "autoUnpack complete check folder"
-    
-    confirmMessageS LOG_TAIL "Confirm Completion"
+    print "autoUnpack complete check folder"
