e56a21cc97faf7153fa094b41450ffa06dbb446b
Miles J. Litteral <Mandaloe2@gmail.com>
2023-02-27 11:39:20 -0800

    ghc versioning hotfix

--- src/megastore.hs
+++ src/megastore.hs
@@ -13,19 +13,23 @@ A Module for taking a directory of images (for example) and turning them into a 
 that will efficiently store all images. Here is a quick crash course:
 
     @
-        a1  <- loadFile "s1.png"
-        a2  <- loadFile "s2.png"
-        a3  <- loadFile "s3.png"
+        a1  <- loadFile "./assets/s1.png"
+        a2  <- loadFile "./assets/s2.png"
+        a3  <- loadFile "./assets/s3.png"
 
-        let testSet = KeyStore [("s1", a1), ("s2", a2), ("s3", a3)]
-        saveStore "./test/testSet" testSet
-
-        loadedContents <- loadStore "./test/testSet.keystore"
-        autoUnpack "./results" loadedContents
+        let testSet = MegaStore [(pack "s1", a1), (pack "s2", a2), (pack "s3", a3)]
+        createDirectoryIfMissing False "./test"
+        saveStore "./test/testSet" testSet -- keystore of images
+        print "Loading MegaStore testSet"
+        loadedContents <- loadStore "./test/testSet.megastore"
+        print "autoUnpack into ./test"
+        autoUnpack "./test" loadedContents
+        print "autoUnpack complete check folder"
     @
 -}
 module MegaStore
-    (  -- * Data Types #Data Type#
+    (  
+    -- * Data Types        #Data Type#
     MegaStore(..)
     -- * I/O Functions     #Functions#
     , saveStore
